8d810a701f2ea2694c1845da86bec0ad541c34e2
JHJ1999 <jacobhejboel@gmail.com>
2020-10-28 13:00:21 +0100

    Første post

--- Model/index.js
+++ Model/index.js
@@ -3,8 +3,11 @@ const cors = require('cors');
 const app = express();
 const port = 3000
 const mongoose = require('mongoose');
+const adminModel = require("../Model/Admin");
+const bodyParser = require('body-parser');
+const { request } = require('express');
 
-mongoose.connect("mongodb+srv://eksamen:eksamen@cluster0.uuj1t.mongodb.net/eksamen?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.connect("mongodb+srv://eksamen:eksamen@cluster0.uuj1t.mongodb.net/Cluster0?retryWrites=true&w=majority", { useNewUrlParser: true, useUnifiedTopology: true });
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
@@ -14,10 +17,30 @@ console.log("database connected");
 
 
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({urlencoded: true}));
 
 app.get('/', (req, res) => {
   res.send('')
 })
+
+app.post('/signup',(req,res) => {
+const newUser = new adminModel({
+      role: "user",
+      email: req.body.email,
+      password: req.body.password
+});
+newUser.save()
+  .then(user =>{
+
+  if(user)
+  {res.status(200).json(user);
+  }
+  })
+  .catch(err =>
+    res.status(500).json({error: err}));
+
+});
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
